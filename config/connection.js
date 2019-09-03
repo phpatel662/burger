6b788e2062a7881b390b35454a6ccd1c56f2de11
ppatel662 <phpatel662@gmail.com>
2019-09-02 20:19:14 -0400

    trying to make it work with heroku

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ if (process.env.JAWSDB_URL) {
   // Database is JawsDB on Heroku
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  mysql.createConnection({
+  connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
